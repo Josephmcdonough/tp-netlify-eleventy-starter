fcd60a787b996c3266d7ec7c0e1a9c5b8543079f
Joseph <josephmcdonough71@gmail.com>
2020-10-02 17:20:04 +0200

    navigation finale v2

--- .eleventy.js
+++ .eleventy.js
@@ -122,7 +122,3 @@ module.exports = function (eleventyConfig) {
   }
 }
 
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-};
